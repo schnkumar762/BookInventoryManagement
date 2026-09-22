70fc527e0bbf58e8b61c93551a3b525cc8b22bf2
Sachin Kumar <schnkumar762@gmail.com>
2026-09-22 17:54:46 +0530

    feat: implement BookService with CRUD methods for book management

--- src/services/book.service.js
+++ src/services/book.service.js
@@ -1,2 +1,20 @@
-class BookService {}
+const BookRepository = require("../repositories/book.repository");
+
+class BookService {
+  constructor() {
+    this.bookRepository = new BookRepository();
+  }
+  async addBook(bookData) {
+    return await this.bookRepository.addBook(bookData);
+  }
+  async getAllBooks() {
+    return await this.bookRepository.getAllBooks();
+  }
+  async getBook(ISBN) {
+    return await this.bookRepository.getBook(ISBN);
+  }
+  async deleteBook(ISBN) {
+    return await this.bookRepository.deleteBook(ISBN);
+  }
+}
 module.exports = BookService;
