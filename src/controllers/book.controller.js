f146b800b014e61a4d0f4e28c2f4a2a2dcd9d001
Sachin Kumar <schnkumar762@gmail.com>
2026-09-22 18:28:06 +0530

    feat: implement BookController and update repository/service exports

---
+++ src/controllers/book.controller.js
@@ -0,0 +1,8 @@
+const { BookService } = require("../services");
+const { BookRepository } = require("../repositories");
+async function addBook(req, res, next) {}
+async function getAllBooks(req, res, next) {}
+async function getBook(req, res, next) {}
+async function deleteBook(req, res, next) {}
+
+module.exports = { addBook, getAllBooks, getBook, deleteBook };
--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  bookRepository: require("./book.repository"),
+  BookRepository: require("./book.repository"),
   authorRepository: require("./author.repository"),
 };
