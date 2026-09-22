936f7ec02c1fc8bf04e4fd45ace066ffc45b72f7
Sachin Kumar <schnkumar762@gmail.com>
2026-09-22 16:36:56 +0530

    refactor: rename router variables for clarity and consistency across routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,12 +3,12 @@ const express = require("express");
 const bookRouter = require("./book.routes");
 const authorRouter = require("./author.routes");
 
-const router = express.Router();
+const v1Router = express.Router();
 
-router.use("/ping", (req, res) => {
+v1Router.use("/ping", (req, res) => {
   res.json({ message: "v1 is working fine" });
 });
-router.use("/books", bookRouter);
-router.use("/authors", authorRouter);
+v1Router.use("/books", bookRouter);
+v1Router.use("/authors", authorRouter);
 
-module.exports = router;
+module.exports = v1Router;
